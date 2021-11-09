fb10642390a6d3f129daf54ecaad92440ef48864
Roberto <roberto.cantu@gmail.com>
2021-11-09 17:46:10 -0600

    Added textures

--- ProyectoFinal/app.js
+++ ProyectoFinal/app.js
@@ -1,10 +1,14 @@
 import * as THREE from './node_modules/three/build/three.module.js'
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
 
 
-  
+
 function main(){
     const canvas = document.querySelector("#root");
     const renderer = new THREE.WebGLRenderer({canvas});
+    
+    document.body.appendChild( renderer.domElement );
+    
 
     //Set up camera
     const fov = 75;
@@ -12,20 +16,29 @@ function main(){
     const near = 0.1;
     const far = 1000;
     const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
-    camera.position.set(4,2.5,7.5); 
+    camera.position.set(0,2.5,7.5); 
+    //camera.position.z = 100
     camera.lookAt(new THREE.Vector3(0,0,0)); 
     console.log(aspect);
     //camera.position.set(0,1.8,-5);
     // camera.lookAt(new THREE.Vector3(0,0,0));
+    //const controls = new OrbitControls( camera, renderer.domElement );
+    //controls.update();
+   
+    //Add orbit controls for better view
+    const controls = new OrbitControls(camera, renderer.domElement);
 
-    //Create scene
     const scene = new THREE.Scene();
     
-
+    //Create scene
     //Create floor
+    let texture = new THREE.TextureLoader().load('img/floor.jpg')
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(4, 4);
     const meshFloor = new THREE.Mesh(
-      new THREE.PlaneGeometry(10,10,10,10),
-      new THREE.MeshBasicMaterial({color:0xffffff, wireframe:true})
+      new THREE.PlaneGeometry(20,15,10,10),
+      new THREE.MeshBasicMaterial({map:texture, wireframe:false})
     );
     meshFloor.rotation.x -= Math.PI /2;
     scene.add(meshFloor);
@@ -34,38 +47,47 @@ function main(){
      const house = new THREE.Group()
      scene.add(house)
 
-  //   //Create house 
+     //Create house 
+     let wallTexture = new THREE.TextureLoader().load('img/wall.jpg');
+     //wallTexture.wrapS = THREE.RepeatWrapping;
+    // wallTexture.wrapT = THREE.RepeatWrapping;
+     //wallTexture.repeat.set(4, 4);
      const walls = new THREE.Mesh(
      new THREE.BoxBufferGeometry(4, 2.5, 4),
-     new THREE.MeshBasicMaterial({ color: '#ac8e82' })
+     new THREE.MeshBasicMaterial({ color: 0xffffff,map:wallTexture})
    )
    walls.position.y = 1.25
    house.add(walls)
 
-   // Roof
-const roof = new THREE.Mesh(
+  // Roof
+  let roofTexture = new THREE.TextureLoader().load('img/roofCabin.jpg')
+  const roof = new THREE.Mesh(
   new THREE.ConeBufferGeometry(3.5, 1, 4),
-  new THREE.MeshBasicMaterial({ color: '#b35f45' })
+  new THREE.MeshBasicMaterial({ color: '#b35f45', map: roofTexture })
 )
 roof.rotation.y = Math.PI * 0.25
 roof.position.y = 2.5 + 0.5
 house.add(roof)
 
 // Door
+let doorTexture = new THREE.TextureLoader().load('img/door.jpg');
 const door = new THREE.Mesh(
   new THREE.PlaneBufferGeometry(2, 2),
-  new THREE.MeshBasicMaterial({ color: '#aa7b7b' })
+  new THREE.MeshBasicMaterial({ color: '#aa7b7b', map:doorTexture })
 )
-door.position.y = 1
+door.position.y = 0.9
 door.position.z = 2 + 0.01
 house.add(door)
+
+//Window
 
 // Graves
 const graves = new THREE.Group()
 scene.add(graves)
 
+let stoneTexture = new THREE.TextureLoader().load('img/stone.png')
 const graveGeometry = new THREE.BoxBufferGeometry(0.6, 0.8, 0.2)
-const graveMaterial = new THREE.MeshBasicMaterial({ color: '#b2b6b1' })
+const graveMaterial = new THREE.MeshBasicMaterial({ color: '#b2b6b1', map: stoneTexture })
 
 for(let i = 0; i < 50; i++)
 {
@@ -85,7 +107,14 @@ for(let i = 0; i < 50; i++)
     grave.rotation.y = (Math.random() - 0.5) * 0.4
 
     // Add to the graves container
-    graves.add(grave)
+    console.log(`x value is ${x}`);
+    //console.log(`z values is ${z}`)
+    if (x > 10 || x < -10 || z > 7.5 || z < -7.5){
+      //Dont add gravee
+    } else {
+      graves.add(grave)
+    }
+   
 }
     //Create geometry
     // const boxWidth = 1;
@@ -115,9 +144,22 @@ for(let i = 0; i < 50; i++)
     //   requestAnimationFrame(render);
     
     //Renderer
-    renderer.render(scene, camera);
+    
+    //animate();
+    //renderer.render(scene, camera);
+    function animate() {
+
+      requestAnimationFrame( animate );
+    
+     // // required if controls.enableDamping or controls.autoRotate are set to true
+      //controls.update();
+     // main()
+      renderer.render( scene, camera );
+      controls.update();
+    }
+    animate();
 }
 
-
-
 main();
+
+
